0c401672bc0f450b26ec1f1659a75f73674f35e8
Codeformyself <prostozabeygf@gmail.com>
2016-04-29 21:00:16 +0300

    addsa

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,26 @@
-$(document).ready(function() {
-	$('.thumb').magnificPopup({
-		type : 'image',
-		gallery : {
-			enabled : true
-		},
-		removalDelay: 300,
-		mainClass: 'mfp-fade'
+$(window).scroll(function () {
+	var st = $(this).scrollTop();
+
+	$(".slider img").css({
+		"transform" : "translate(0%, -" + st /20 + "%"
 	});
 
+});
+
+$(function () {
+	$(".menu").click(function(){
+		$(".nav").toggleClass('show-in');
+		$(".select-c").toggleClass('show-in');
+	});
+ 
+});
+
+$('#myTab a').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+})
+
+$(document).ready(function() {
 $('a[href^="#"]').click(function(){
 var el = $(this).attr('href');
 $('body').animate({
